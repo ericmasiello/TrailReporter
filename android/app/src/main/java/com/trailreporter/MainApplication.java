c43a26159bbdc1e9d914a961f53fea9dd8d616d1
Eric Masiello <eric.j.masiello@gmail.com>
2016-08-03 20:13:27 -0400

    Initial commit with app configured

--- android/app/src/main/java/com/trailreporter/MainApplication.java
+++ android/app/src/main/java/com/trailreporter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pepperoniapptemplate;
+package com.trailreporter;
 
 import android.app.Application;
 import android.util.Log;
--- ios/TrailReporterTests/TrailReporterTests.m
+++ ios/TrailReporterTests/TrailReporterTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface TrailReporterTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation TrailReporterTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
